8a0a6b4ea59dd7df84e0905a95d31dad15f30696
japostolidis <japostolidis@gmail.com>
2021-12-18 18:28:25 +0200

    added deployment scripts2

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t japostolidis/multi-client:latest -t japostolidis/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t japostolidis/multi-server:latest -t japostolidis/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t japostolidis/multi-worker:latest -t japostolidis/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t japostolidis/multi-client-k8s:latest -t japostolidis/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t japostolidis/multi-server-k8s-pgfix:latest -t japostolidis/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t japostolidis/multi-worker-k8s:latest -t japostolidis/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push japostolidis/multi-client:latest
-docker push japostolidis/multi-server:latest
-docker push japostolidis/multi-worker:latest
+docker push japostolidis/multi-client-k8s:latest
+docker push japostolidis/multi-server-k8s-pgfix:latest
+docker push japostolidis/multi-worker-k8s:latest
 
-docker push japostolidis/multi-client:$SHA
-docker push japostolidis/multi-server:$SHA
-docker push japostolidis/multi-worker:$SHA
+docker push japostolidis/multi-client-k8s:$SHA
+docker push japostolidis/multi-server-k8s-pgfix:$SHA
+docker push japostolidis/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=japostolidis/multi-server:$SHA
-kubectl set image deployments/client-deployment client=japostolidis/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=japostolidis/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=japostolidis/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=japostolidis/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=japostolidis/multi-worker-k8s:$SHA
